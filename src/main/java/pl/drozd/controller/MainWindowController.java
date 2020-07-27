1d51528a517fcf109ba72a576aa49d7cd21662a1
Dominik Drozd <dominik_drozd@vp.pl>
2020-07-27 10:34:05 +0200

    Add main structure of project

---
+++ src/main/java/pl/drozd/controller/MainWindowController.java
@@ -0,0 +1,10 @@
+package pl.drozd.controller;
+
+import pl.drozd.view.ViewFactory;
+
+public class MainWindowController extends BaseController {
+
+    public MainWindowController(ViewFactory viewFactory, String fxmlName) {
+        super(viewFactory, fxmlName);
+    }
+}
