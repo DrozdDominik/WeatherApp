5dc22c8f68616649c40c29772838772a00c1547a
Dominik Drozd <dominik_drozd@vp.pl>
2020-07-27 15:33:16 +0200

    Add errors handler class

---
+++ src/main/java/pl/drozd/util/Errors.java
@@ -0,0 +1,12 @@
+package pl.drozd.util;
+
+import javafx.scene.control.Label;
+
+public class Errors {
+
+public static void showErrorMessage(Label label, String message){
+
+    label.setText(message);
+}
+
+}
