9cd4dfa15ed192471579ee2768b32249a5e1f4ca
Dominik Drozd <dominik_drozd@vp.pl>
2020-07-27 13:14:34 +0200

    Add weather API

---
+++ src/main/java/pl/drozd/config/ConfigAPI.java
@@ -0,0 +1,10 @@
+package pl.drozd.config;
+
+public class ConfigAPI {
+
+    private final String apiKey ="eacc1ba16c3d4879108b375d1301fc10";
+
+    public String getApiKey(){
+        return  apiKey;
+    }
+}
